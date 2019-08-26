4e7c208162e047968fc46ada0543c23ad968dd37
Ryan Lacon <ryan.lacon@gmail.com>
2019-08-26 19:46:52 -0400

    CardData state changes to true if gets clicked and current state is false. Resets card states after every clicky and increases counter.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,16 +3,16 @@ import React from "react";
 function Cards(props) {
     return (
         <div style={{ justifyContent: 'center', alignItems: 'center', padding: 10, display: 'inline-block', width: '25%', textAlign: 'center' }}>
-            <img onClick={() => props.increaseScore()} style={{ cursor: 'pointer' }} alt={props.id} src={props.url} />
+            <img
+                style={{ cursor: 'pointer' }}
+                alt={props.id}
+                src={props.url}
+                onClick={() => {
+                    props.loseGame(props.id)
+                    props.increaseScore()
+                }}
+            />
         </div>
-
-
-        // role="img"
-        // aria-label="click-item"
-        // onClick={() => props.handleClick(props.id)}
-        // style={{ backgroundImage: `url("${props.url}")` }}
-        // style={{ backgroundImage: `url("${props.url}")` }}
-        // className={`click-item${props.random ? " random" : ""}`}
     );
 }
 
--- src/components/Game.js
+++ src/components/Game.js
@@ -5,6 +5,7 @@ import CardData from "../CardData.json";
 import Jumbotron from "./Jumbotron";
 
 class Game extends Component {
+
     state = {
         CardData,
         count: 0
@@ -14,23 +15,46 @@ class Game extends Component {
         this.setState({ count: this.state.count + 1 });
     };
 
-    // Need a function to shuffle the cards, handle if a user clicked a card, if the user won or lost after every card click, rest the game after the user loses
-    render() {
-        return (
-            // Will need key, id, click function, and images 
-            <div>
+    // Causes the state of the CardData object to flip from false to true
+    loseGame = id => {
+        this.state.CardData.map(item => {
+            const newItem = { ...item };
+            if (newItem.id === id) {
+                if (newItem.clicked === false) {
+                    newItem.clicked = true;
+                }
+            }
+            // console.log(this.state.CardData)
+            console.log(newItem)
+            console.log(id)
+            return newItem;
+        });
+    }
 
+    // winGame = id => {}
+
+    // Need a function to shuffle the cards, if the user won or lost after every card click, reset the game after the user loses
+    render(props) {
+        return (
+            <div>
                 <Jumbotron />
                 <nav className="navbar navbar-light bg-light">
-                    <a className="navbar-brand" href="http://rlacon.github.io/ClickyGame">Clicky Game</a>
-                    <p id="score-text">Score: {this.state.count}</p>
-                    <p id="top-score-text">0</p>
-                </nav>                
+                    <p id="score-text">Current Score: {this.state.count}</p>
+                    <p id="top-score-text">High Score: 0</p>
+                </nav>
                 <CardContainer>
                     {
-                        this.state.CardData.map(item => {
-                            return <Cards style={{ cursor: 'pointer' }} increaseScore={this.increaseScore} key={item.id} id={item.id} url={item.url} />
-                        })
+                        this.state.CardData.map(item => (
+                            // (Parantheses not needed if rendering an object)
+                            <Cards
+                                style={{ cursor: 'pointer' }}
+                                increaseScore={this.increaseScore}
+                                key={item.id}
+                                id={item.id}
+                                url={item.url}
+                                loseGame={this.loseGame}
+                            />
+                        ))}
                     }
                 </CardContainer>
             </div>
