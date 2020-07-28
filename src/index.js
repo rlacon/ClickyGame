38dde8fb79af8344a2db5c67105c3474b13678de
Ryan Lacon <ryan.lacon@gmail.com>
2020-07-28 18:55:10 -0400

    Removed unused npm packages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './fonts/FOT-Seurat-Pro-B.woff'
 
 ReactDOM.render(
   <React.StrictMode>
