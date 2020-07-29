566e5075b87bc4810bba41b6373a0e1c0d29a226
Ryan Lacon <ryan.lacon@gmail.com>
2020-07-28 20:30:33 -0400

    heroku

---
+++ build/precache-manifest.34506de69f1eee00572a6204f6962f09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5a7e669400802328aa640a53edf776",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bb503257d5c3b2d0fb0",
+    "url": "/static/css/main.50f0a582.chunk.css"
+  },
+  {
+    "revision": "a2ceba2245c3f5d11f3a",
+    "url": "/static/js/2.083df994.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.083df994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb503257d5c3b2d0fb0",
+    "url": "/static/js/main.67423a37.chunk.js"
+  },
+  {
+    "revision": "22eabea025eba006b39b",
+    "url": "/static/js/runtime-main.34b1f622.js"
+  },
+  {
+    "revision": "bbd50caafb6d6e8a9ba22c3192e53bc9",
+    "url": "/static/media/AnimalCrossingTitle.bbd50caa.png"
+  }
+]);
